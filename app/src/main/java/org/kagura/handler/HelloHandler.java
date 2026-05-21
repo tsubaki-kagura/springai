ae54eb2bfb4e2350f72c66cd7f145337d9cce4ad
Tsubaki Kagura <tsubaki.kagura@outlook.com>
2026-05-21 17:00:46 +0800

    使用 prompt 封装用户消息
    1. 自定义模板渲染器，使用 <> 作为占位符
    2. 使用链式 api 结合用户消息构建 prompt
    3. 将阻塞的结构化输出使用调度器调度到其它线程上，防止工作线程阻塞

--- app/src/main/java/org/kagura/handler/HelloHandler.java
+++ app/src/main/java/org/kagura/handler/HelloHandler.java
@@ -1,45 +1,52 @@
 package org.kagura.handler;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.metadata.DefaultUsage;
-import org.springframework.ai.chat.metadata.Usage;
-import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
-@Slf4j
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class HelloHandler {
     private final ChatClient chatClient;
 
+    @Value("classpath:prompts/system.st")
+    private Resource system;
+
+    @Value("classpath:prompts/user.st")
+    private Resource user;
+
     public Mono<ServerResponse> hello(ServerRequest request) {
         return request.bind(UserChatRequest.class)
-                .map(UserChatRequest::message)
                 .flatMapMany(this::chat)
-                .as(result -> ServerResponse.ok().body(result, String.class));
+                .as(result -> ServerResponse.ok().body(result, Person.class));
     }
 
-    private Flux<String> chat(String message) {
-        return chatClient.prompt()
-                .user(message)
-                .stream()
-                .chatResponse()
-                .doOnNext(chatResponse -> {
-                    Usage usage = chatResponse.getMetadata().getUsage();
-                    if (usage instanceof DefaultUsage defaultUsage) {
-                        log.debug("default usage: {}", defaultUsage);
-                    }
-                })
-                .mapNotNull(ChatResponse::getResult)
-                .mapNotNull(result -> result.getOutput().getText());
+    private Flux<Person> chat(UserChatRequest userChatRequest) {
+        return Mono.fromCallable(() -> chatClient.prompt()
+                        .system(system, StandardCharsets.UTF_8)
+                        .user(spec -> spec.text(user, StandardCharsets.UTF_8)
+                                .param("count", userChatRequest.count))
+                        .call()
+                        .<List<Person>>entity(new ParameterizedTypeReference<>() {
+                        }))
+                .subscribeOn(Schedulers.boundedElastic())
+                .flatMapMany(Flux::fromIterable);
     }
 
-    private record UserChatRequest(String message) {
+    private record UserChatRequest(Integer count) {
+    }
+
+    private record Person(String name, Integer age) {
     }
 }
